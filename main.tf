3c77182851b413964102e70fa4bbba5d7db2137f
jeferson <jefersonlinuxextreme@gmail.com>
2023-03-29 20:11:59 -0300

    added

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "github-lab-test"
 
     workspaces {
       name = "learn-terraform-github-actions"
